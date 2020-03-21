2153f56195fb52c158ef5bd4da762415df9add4b
heymp <mgp140@gmail.com>
2020-03-21 11:24:33 -0400

    added init export

--- hasura/migrations/1584803474324_init/up.sql
+++ hasura/migrations/1584803474324_init/up.sql
@@ -14,7 +14,7 @@ CREATE TABLE public.builds (
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     updated_at timestamp with time zone DEFAULT now() NOT NULL,
     dependencies jsonb,
-    status text DEFAULT 'init'::text NOT NULL
+    status text DEFAULT 'INIT'::text NOT NULL
 );
 CREATE SEQUENCE public.builds_id_seq
     AS integer
@@ -24,8 +24,18 @@ CREATE SEQUENCE public.builds_id_seq
     NO MAXVALUE
     CACHE 1;
 ALTER SEQUENCE public.builds_id_seq OWNED BY public.builds.id;
+CREATE TABLE public.builds_statuses (
+    value text NOT NULL,
+    description text NOT NULL
+);
+-- Insert builds_statuses enums
+INSERT INTO builds_statuses (value, description) VALUES ('INIT', 'This default state of the build, when it has been first created.'), ('RECIEVED', 'The event has been recieved by the worker'), ('COMPLETED', 'The build has completed.'), ('ERROR', 'There was an issue with the build');
 ALTER TABLE ONLY public.builds ALTER COLUMN id SET DEFAULT nextval('public.builds_id_seq'::regclass);
 ALTER TABLE ONLY public.builds
     ADD CONSTRAINT builds_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.builds_statuses
+    ADD CONSTRAINT builds_statuses_pkey PRIMARY KEY (value);
 CREATE TRIGGER set_public_builds_updated_at BEFORE UPDATE ON public.builds FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
 COMMENT ON TRIGGER set_public_builds_updated_at ON public.builds IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+ALTER TABLE ONLY public.builds
+    ADD CONSTRAINT builds_status_fkey FOREIGN KEY (status) REFERENCES public.builds_statuses(value) ON UPDATE RESTRICT ON DELETE RESTRICT;
